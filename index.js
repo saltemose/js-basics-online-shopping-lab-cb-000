04330b23f3ca93527e6956b68180c9f9c56d30a1
Sean Altemose <sean@seanaltemose.com>
2019-04-24 20:23:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -65,6 +65,7 @@ function placeOrder(cardNumber) {
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   }
   else {
+    var cost = total()
     return "Sorry, we don't have a credit card on file for you."
   }
 }

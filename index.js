c8121785e1d53703c8171b1331bf558256143930
Sean Altemose <sean@seanaltemose.com>
2019-04-24 20:22:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ function placeOrder(cardNumber) {
   // write your code here
   if (cardNumber){
     cart = [];
-    return `Your total cost is $${cart.total()}, which will be charged to the card ${cardNumber}.}`
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.}`
   }
   else {
     return "Sorry, we don't have a credit card on file for you."

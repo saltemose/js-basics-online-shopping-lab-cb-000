869952220ed92f4fe082259d3644990720b1d935
Sean Altemose <sean@seanaltemose.com>
2019-04-24 20:15:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ function removeFromCart(item) {
   var i=0;
   for (i=0; i < cart.length; i++){
       if (cart[i].itemName == item) {
-      cart.splice(cart[i]);
+      cart.splice(i, 1);
       return cart;
     }
     break

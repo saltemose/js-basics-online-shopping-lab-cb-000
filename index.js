65575714bcf866a9a37bb308a96436979d518836
Sean Altemose <sean@seanaltemose.com>
2019-04-24 19:56:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
- var item = {itemName: item, itemPrice: Math.floor(Math.random()*100)}
+ var obj = {itemName: item, itemPrice: Math.floor(Math.random()*100)}
  cart.push(item);
  return `${item} has been added to your cart.`;
 }

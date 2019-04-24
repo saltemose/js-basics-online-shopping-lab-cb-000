af9cb8237f91352775c3ba52c7dbaf7608604618
Sean Altemose <sean@seanaltemose.com>
2019-04-24 20:04:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function viewCart() {
   for (i=0; i < cart.length - 1 ; i++) {
     cartItems.push(`${cart[i].itemName} at ${cart[i].itemPrice}`)
   }
-    cartItems.push(`${cart[cart.length-1].itemName} at ${cart[cart.length-1].itemPrice}`)
+    cartItems.push(`${cart[cart.length-1].itemName} at $${cart[cart.length-1].itemPrice}`)
     cartItems.join(", ")
     statement.push(cartItems)
       return statement.join("")
